1b4ed98bc09202df853d07403accfd22492a4e78
Otávio Caporal Dev <otaviocaporal@gmail.com>
2026-05-13 20:59:39 -0300

    criação classe Musica.java

--- src/atividade2/Musica.java
+++ src/atividade2/Musica.java
@@ -1,4 +1,55 @@
 package atividade2;
 
 public class Musica {
+    private String titulo;
+    private String artista;
+    private String album;
+    private int duracao;
+
+    public Musica(String titulo, String artista, String album, int duracao) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.album = album;
+        this.duracao = duracao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    @Override
+    public String toString() {
+        return "Música: " + titulo +
+                "\nArtista: " + artista +
+                "\nÁlbum: " + album +
+                "\nDuração: " + duracao + " segundos";
+    }
 }
